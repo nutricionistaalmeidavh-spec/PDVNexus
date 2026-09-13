86a71949dc1d263f756f5d7a5f303ab85955163e
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-13 01:00:38 -0300

    feat(ui): wire Lote D theme into Windows 7 PDV

--- windows-7/apps/pdv-demo/src/main.tsx
+++ windows-7/apps/pdv-demo/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PdvDemoApp } from "./PdvDemoApp";
+import "./lote-d-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
