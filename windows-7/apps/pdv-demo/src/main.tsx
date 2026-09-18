1417e7a402b8e0a165c11e4f54d39f472fa25b3f
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-18 12:17:42 -0300

    feat(pdv): montar FEFO no legado

--- windows-7/apps/pdv-demo/src/main.tsx
+++ windows-7/apps/pdv-demo/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { LabelBatchTools } from "./LabelBatchTools";
 import { PdvDemoApp } from "./PdvDemoApp";
+import { ProductBatchFefoSync } from "./ProductBatchFefoSync";
 import { SaleObservationDecor } from "./SaleObservationDecor";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <>
       <SaleObservationDecor />
+      <ProductBatchFefoSync />
       <LabelBatchTools />
       <PdvDemoApp />
     </>
